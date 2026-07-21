c9e300039b82d2e1a5eacd054cf7f94119cc1c84
rexlevin <56460263@qq.com>
2026-07-21 17:14:38 +0800

    style：全局页面禁用文本选中功能

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@ body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
     font-size: 15px;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
 }
 #app {
     width: 100vw;
